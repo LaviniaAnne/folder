7aab46f0eefb966c5a9f48a56cdc469ee9273763
LaviniaAnne <lavinia.godwin@wipro.com>
2016-07-25 16:11:54 +0530

    Add files via upload

---
+++ SpringBatchExample/src/main/java/com/samplebatch/Domain.java
@@ -0,0 +1,86 @@
+package com.samplebatch;
+
+public class Domain {
+
+	private String name;
+	private String  address;
+	private String dob;
+	private String email;
+	private String  zip;
+	private String country;
+	private String state;
+	private String account;
+	private String credit;
+	private String mobileno;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+		System.out.println("country");
+
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+		System.out.println("STATE");
+
+	}
+	public String getAccount() {
+		return account;
+		
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getCredit() {
+		return credit;
+	}
+	public void setCredit(String credit) {
+		this.credit = credit;
+		System.out.println("credit");
+
+	}
+	public String getMobileno() {
+		return mobileno;
+	}
+	public void setMobileno(String mobileno) {
+		this.mobileno = mobileno;
+	}
+	
+	}
+	
+
